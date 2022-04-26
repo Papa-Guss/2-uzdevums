3a1fd293a26a21205f99bcb6a9ced766785d9f76
Papa-Guss <77328472+Papa-Guss@users.noreply.github.com>
2022-04-26 12:39:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a = int(input("Ievadiet pirmo skaitli: "))
+b = int(input("Ievadiet otro numuru: "))
+if a == 0:
+  print("Ar nullu dalit nedrikst!")
+elif b == 0:
+  print("Ar nulli dalit nedrikst!")
+else:
+  print(a // b)
